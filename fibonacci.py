2cdd41e26099a57cc96ed2b44e7694ddfc9bed89
DanielRiepl <daniel.riepl@dbb.su.se>
2023-05-31 00:34:52 +0200

    unfinished mess

---
+++ fibonacci.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+
+def fibonacci(num):
+    if (num == 1):
+        return 0
+    elif (num == 2):
+        return 1
+    else:
+        fib = 1
+        for i in range(1, num+1):       
+            fib += 
+    return
+
+
+fibonacci(3)
+
+
+# In[ ]:
+
+
+
+
